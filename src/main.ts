7351361eb776eade9fa3c2f3ff64e98a12f50ccf
felipecorzin <jfelipestc@gmail.com>
2024-11-24 21:04:26 +0100

    creada ruta public

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
 
   app.setGlobalPrefix('api/v1');
 
@@ -14,6 +18,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Directorio Público
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   await app.listen(3000, () => {
     console.log('Servidor corriendo en puerto 3000')
